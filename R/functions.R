3c13ce7564c6940a354a9852d88d1999f844c9bb
biscarri1 <wbiscar2@illinois.edu>
2017-10-14 15:34:31 -0400

    Update functions.R

--- R/functions.R
+++ R/functions.R
@@ -1,63 +1,80 @@
-dconvpoibin <- function(p,method="direct",M=2){
+##' Compute the Poisson binomial distribution via convolution
+##' 
+##' The cdf and pmf for the Poisson binomial distribution computed using
+##' convolution methods
+##' 
+##' See the reference for computational details.
+##'
+##' @aliases ppoibin dpoibin
+##' @param p The vector of success probabilities
+##' @param method "direct" for the direct convolution method, "fft_tree" for
+##' the binary tree fft convolution method.
+##' @param M The number of splits for the tree used in the fft_tree
+##' convolution.
+##' @return Returns the entire cdf or pmf
+##' @export
+##' @rdname convpoibin
+##' @author
+##' 
+##' William Biscarri
+##' @references test test test
+##' @examples
+##' 
+##'   p=runif(100)
+##'   ppoibin(p,method='direct',M=2)
+##'   ppoibin(p,method='fft_tree',M=2)
+##'   dpoibin(p,method='direct',M=2)
+##'   dpoibin(p,method='fft_tree',M=2)
+##'   
+##'   p=runif(1000)
+##'   ppoibin(p,method='direct',M=2)
+##'   ppoibin(p,method='fft_tree',M=4)
+##'   dpoibin(p,method='direct',M=2)
+##'   dpoibin(p,method='fft_tree',M=4)
+##'
+dpoibin <- function(p,method="direct",M=2){
   
   lp = as.integer(length(p))
+  total_size = lp+1
   
   switch(method,
          "direct"={
-           
-           total_size = lp+1
-           
-           density = .C('direct_convolution', as.double(p),as.integer(lp),as.double(vector("double",total_size)))[[3]]
-           
+           density = .C('direct_convolution', as.double(p),lp,as.double(vector("double",total_size)))[[3]]
          },
-         
          "fft_tree" = {
-                     
            num_splits = as.integer(M)
-           
-           result = as.double(vector("double",lp+num_splits))
-           
-           density  = .C('tree_convolution',p,lp,num_splits,result)[[4]][1:(lp+1)]
-           
-           
-           
+           density  = .C('tree_convolution',as.double(p),lp,num_splits,
+                         as.double(vector("double",lp+num_splits)))[[4]][1:total_size]
          }
-  )
+        )
   
   return(density)
-  
 }
 
 
-
-pconvpoibin <- function(p,method="direct",M=2){
+##' @export
+##' @rdname convpoibin
+ppoibin <- function(p,method="direct",M=2){
   
   lp = as.integer(length(p))
-  
+  total_size = lp+1
+  #NOTE: for some reason, saving result and then doing return(result) causes an error
+  #however, doing return(cumsum(density)) works, however. Need to understand why.
   switch(method,
          "direct"={
-           
-           total_size = lp+1
-           
-           density = .C('direct_convolution', as.double(p),as.integer(lp),as.double(vector("double",total_size)))[[3]]
-           
-           result = cumsum(density)
-
+           density = .C('direct_convolution', as.double(p),lp,as.double(vector("double",total_size)))[[3]]
+           #result = cumsum(density)
+           #density=0
          },
          
          "fft_tree" = {
-           
            num_splits = as.integer(M)
-           
-           result = as.double(vector("double",lp+num_splits))
-           
-           density  = .C('tree_convolution',p,lp,num_splits,result)[[4]]
-           
-           result = cumsum(density[1:(lp+1)])
-           
+           density  = .C('tree_convolution',as.double(p),lp,num_splits,
+                           as.double(vector("double",total_size)))[[4]][1:total_size]
+           #result = cumsum(density)
+           #density=0
          }
-  )
+        )
   
-  return(result)
-  
+  return(cumsum(density))
 }
